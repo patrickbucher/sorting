b5d1e8040f9667d4438ec609578fdb034726ca39
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-05-02 16:17:13 +0200

    optimized BubbleSort

--- main/sorting/BubbleSort.java
+++ main/sorting/BubbleSort.java
@@ -4,10 +4,13 @@ public class BubbleSort<T extends Comparable<T>> implements Sort<T> {
 
     @Override
     public void sort(T[] items) {
-        for (int i = 0; i < items.length; i++) {
+        boolean sorted = false;
+        for (int i = 0; !sorted && i < items.length; i++) {
+            sorted = true;
             for (int j = 0; j < items.length; j++) {
                 if (items[i].compareTo(items[j]) < 0) {
                     SortUtils.swap(items, i, j);
+                    sorted = false;
                 }
             }
         }
